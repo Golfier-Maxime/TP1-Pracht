6a9dd8062c507b8cc01e10fcbc4f2854ab978c43
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2022-09-12 09:19:09 +0200

    fix

--- src/pages/ListeFetch.vue
+++ src/pages/ListeFetch.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Card from "../components/card.vue";
-const res = await fetch("./maison.json")
-const posts = await res.json()
+const res = await fetch("./maison.json");
+const posts = await res.json();
 </script>
     
 <template>
+    <div class="flex flex-col">
     <Card v-for="maison in posts" :key="maison" v-bind="maison"/>
+    </div>
 </template>
